98d92c0f9cd36ffe4059116c1bc034d1f55580d3
Parth Pradhan <parthdbz24@gmail.com>
2022-02-10 13:37:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect,useRef} from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [quotes,setQuotes] = useState('');
+  const textRef = useRef();
+  let colors =["#ffff00","#90ee90","ffa500","#ff68ff","a9a9e7"];
+  const getQuotes = () =>{
+    fetch("https://type.fit/api/quotes")
+    .then(res => res.json())
+    .then((data) => {
+      let randomNum = Math.floor(Math.random()* data.length);
+      setQuotes(data[randomNum]);
+    });
+  };
+  useEffect(()=>{
+    getQuotes();
+  },[]);
+  useEffect(()=>{
+    textRef.current.style.color =colors[Math.floor(Math.random()*colors.length)];
+  },[quotes])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+      <div className="quote">
+        <p ref={textRef}>{quotes.text}</p>
+        <p>Author: {quotes.author}</p>
+        <div className="btnContainer">
+          <button className="btn" onClick={getQuotes}>Get Quotes</button>
+          <a 
+          href={`https://twitter.com/intent/tweet?text=${quotes.text}`}
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          rel="noopener noreferrer" 
+          className="btn">
+            Tweet</a>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
